5f3a41c066e6db8e7937fc16edf3646563cd2e04
Adam Atkinson <mail@adama.dev>
2024-01-01 17:35:34 -0500

    finalize encoding

--- data/oai_summarization/prepare.py
+++ data/oai_summarization/prepare.py
@@ -26,4 +26,28 @@ for fname in os.listdir(datadir):
     except:
         continue
 
-print(len(all_samples))
+np.random.seed(2024)
+train_ixs = np.random.choice(range(len(all_samples)), int(0.9*len(all_samples)), replace=False)
+val_ixs = [ix for ix in range(len(all_samples)) if ix not in set(train_ixs)]
+
+def stringify_datum(ix):
+    return f"<|article|> {all_samples[ix]['article']} <|summary|> {all_samples[ix]['summary']}"
+
+train_data = "\n".join([stringify_datum(ix) for ix in train_ixs])
+val_data = "\n".join([stringify_datum(ix) for ix in val_ixs])
+
+enc = tiktoken.get_encoding("gpt2")
+
+train_ids = enc.encode_ordinary(train_data)
+val_ids = enc.encode_ordinary(val_data)
+print(f"train has {len(train_ids):,} tokens")
+print(f"val has {len(val_ids):,} tokens")
+
+# export to bin files
+train_ids = np.array(train_ids, dtype=np.uint16)
+val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
+val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+
+# train has 381,946 tokens
+# val has 45,262 tokens
